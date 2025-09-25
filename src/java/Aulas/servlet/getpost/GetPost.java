512c8246d7dd1e3cd6d20accf9a5ef675d4c5d8e
MateusCosta159 <153222991+MateusCosta159@users.noreply.github.com>
2025-09-25 07:14:55 -0300

    Update GetPost.java

--- src/java/Aulas/servlet/getpost/GetPost.java
+++ src/java/Aulas/servlet/getpost/GetPost.java
@@ -10,26 +10,28 @@ import jakarta.servlet.http.HttpServletResponse;
 public class GetPost extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String campoA = request.getParameter("campoA"); 
-        String opcaoA = request.getParameter("opcapA"); 
-        String opcaoB = request.getParameter("opcaoB"); 
+    throws ServletException, IOException {
         
-        System.out.println("Campo A " + campoA);
-        System.out.println("Opção A " + opcaoA);
-        System.out.println("Opção B " + opcaoB);
-   }
+        String campoA = request.getParameter("campoA");
+        String opcaoA = request.getParameter("opcaoA");
+        String opcaoB = request.getParameter("opcaoB");
+        
+        System.out.println( " campoA : " + campoA );
+        System.out.println( " opcaoA : " + opcaoA );
+        System.out.println( " opcaoB : " + opcaoB );
+        
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
-
-
+    
 }
